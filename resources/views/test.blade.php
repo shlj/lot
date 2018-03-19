314f88719d4002791f8c598cf4b89e543fa4a3d0
songhang <hang.song02@ele.me>
2018-03-19 17:29:48 +0800

    js

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -4,7 +4,7 @@
 
   <div>{!! $chart->container() !!}</div>
 
-{{-- <script src="path/to/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>--}}
+ <script src="/js/Charts.js" charset="utf-8"></script>
  {!! $chart->script() !!}
  </body>
 </html>

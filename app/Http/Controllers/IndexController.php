0d326ca013525f7aa927aacc1e504f2634cbfa78
songhang <hang.song02@ele.me>
2018-03-19 18:04:33 +0800

    color

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -14,14 +14,10 @@ class IndexController
     public function index()
     {
         $chart = new SampleChart();
-        $chart ->title("My Cool Chart")
-            // A dimension of 0 means it will take 100% of the space
-//            ->dimensions(0, 400) // Width x Height
-            // This defines a preset of colors already done:)
-//            ->template("material")
-            // You could always set them manually
-            // ->colors(['#2196F3', '#F44336', '#FFC107'])
-            // Setup the diferent datasets (this is a multi chart)
+        $chart->labels([1,2,3])
+            ->options([
+                'color' => ['#000000','#FFFFFF','#666666'],
+            ])
             ->dataset('Element 1', 'line', [5,20,100]);
 
         return view('test', ['chart' => $chart]);

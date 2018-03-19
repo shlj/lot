c355d4cb7bb2459c1892ff5fc171019e0be1d383
songhang <hang.song02@ele.me>
2018-03-19 17:08:44 +0800

    update

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,27 +7,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Charts\SampleChart;
 use Charts;
 
 class IndexController
 {
     public function index()
     {
-        $chart = Charts::multi('bar', 'material')
-            // Setup the chart settings
-            ->title("My Cool Chart")
+        $chart = new SampleChart();
+        $chart ->title("My Cool Chart")
             // A dimension of 0 means it will take 100% of the space
-            ->dimensions(0, 400) // Width x Height
+//            ->dimensions(0, 400) // Width x Height
             // This defines a preset of colors already done:)
-            ->template("material")
+//            ->template("material")
             // You could always set them manually
             // ->colors(['#2196F3', '#F44336', '#FFC107'])
             // Setup the diferent datasets (this is a multi chart)
-            ->dataset('Element 1', [5,20,100])
-            ->dataset('Element 2', [15,30,80])
-            ->dataset('Element 3', [25,10,40])
-            // Setup what the values mean
-            ->labels(['One', 'Two', 'Three']);
+            ->dataset('Element 1', 'line', [5,20,100]);
 
         return view('test', ['chart' => $chart]);
     }

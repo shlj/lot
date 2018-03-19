d8409af99bcb9c5afafbee231c6854ca07efc0a4
songhang <hang.song02@ele.me>
2018-03-19 14:48:21 +0800

    update

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -13,13 +13,22 @@ class IndexController
 {
     public function index()
     {
-        $chart = Charts::create('line', 'highcharts')
-            ->setView('custom.line.chart.view')
-            ->setTitle('My nice chart')
-            ->setLabels(['First', 'Second', 'Third'])
-            ->setValues([5,10,20])
-            ->setDimensions(1000,500)
-            ->setResponsive(false);
+        $chart = Charts::multi('bar', 'material')
+            // Setup the chart settings
+            ->title("My Cool Chart")
+            // A dimension of 0 means it will take 100% of the space
+            ->dimensions(0, 400) // Width x Height
+            // This defines a preset of colors already done:)
+            ->template("material")
+            // You could always set them manually
+            // ->colors(['#2196F3', '#F44336', '#FFC107'])
+            // Setup the diferent datasets (this is a multi chart)
+            ->dataset('Element 1', [5,20,100])
+            ->dataset('Element 2', [15,30,80])
+            ->dataset('Element 3', [25,10,40])
+            // Setup what the values mean
+            ->labels(['One', 'Two', 'Three']);
+
         return view('test', ['chart' => $chart]);
     }
 }
